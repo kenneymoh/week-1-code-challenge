80ee3622974acd867e8ad306eff9ac50cb08c195
kennedy maina <kenneyrit@gmail.com>
2022-11-28 14:58:23 +0300

    added grades

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -2,7 +2,7 @@
 
 // A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 
-function gradingGenerator(){
+function studentGradingGenerator(){
     let grade = 99;
  
     // Nested if else
@@ -23,4 +23,4 @@ function gradingGenerator(){
                     }
                     
     };
-gradingGenerator()
+studentGradingGenerator()
